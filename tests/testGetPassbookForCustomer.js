07806c5e96a755658bdad5236b85bf3a9a94d5d4
Boni Gopalan <boni@entelligentsia.in>
2019-08-19 20:25:50 +0530

    adding passbook api example

---
+++ tests/testGetPassbookForCustomer.js
@@ -0,0 +1,39 @@
+const authenticatiion = require('../auth/authenticate.js');
+
+var testGetRate = function () {
+    authenticatiion.authenticateClient(function (err, client) {
+        if (client) {
+            function callback(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(JSON.stringify(body));
+                }
+            }
+            getRate(client, callback);
+        }
+        else {
+            console.error(err);
+        }
+    })
+}
+
+var getRate = function (client, callback) {
+    const extCustomerId = "EXTCUST01";
+    const additionalParametrs = {}    
+    client.invokeApi(null, `/customers/${extCustomerId}/passbook`, 'GET',additionalParametrs)
+        .then(function (result) {
+            console.log(result.data)
+        })
+        .catch(function (result) {
+            if (result.response) {
+                console.dir({
+                    status: result.response.status,
+                    statusText: result.response.statusText,
+                    data: result.response.data
+                });
+            } else {
+                console.log(result.message);
+            }
+        });
+}
+
+testGetRate();
