2c6b0c18d85e493bdaa49c6dbce61dfe3d854776
Boni Gopalan <boni@entelligentsia.in>
2019-09-19 17:43:03 +0530

    Upload client implementation

---
+++ tests/upload/testUploadCustomerDoc.js
@@ -0,0 +1,22 @@
+
+const authenticatiion = require('../../auth/authenticate.js');
+const extCustomerId = 'BG1234567-000';
+const uploadfilePath = 'tests/res/customer.png';
+const uploadclient = require('./uploadclient');
+(
+    async ()=>{
+        let apiClient = await authenticatiion.authenticateClientAsync();                
+        let result = await uploadclient.uploadClientDocument(apiClient,extCustomerId,uploadfilePath);
+        console.log(result);
+        let readableUrl = await uploadclient.getReadableUrl(apiClient,result.fetchurl)
+        console.log(readableUrl);
+        return 0;
+    }
+)()
+.then(()=>{
+    process.exit(0)
+})
+.catch(e=>{
+    console.error(e.message);
+})
+
