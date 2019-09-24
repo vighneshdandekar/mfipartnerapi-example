f035aa170a15dd719528fb1b764932f20d194cf6
Boni Gopalan <boni@entelligentsia.in>
2019-09-24 17:41:00 +0530

    Added SIP LIST, GET and DELETE test cases.

---
+++ tests/customers/sips/testGetSipsForACustomer.js
@@ -0,0 +1,11 @@
+const authenticatiion = require('../../../auth/authenticate.js');
+const extCustomerId = 'BMFIBR001CST001';
+(async () =>{
+    let apiClient = await authenticatiion.authenticateClientAsync();
+    let result = apiClient.invokeApi(null,`/customers/${extCustomerId}/sips`,'GET',{},{});
+    return result;
+})()
+.then(result=>{
+    console.log(result);
+    process.exit(0);
+})
