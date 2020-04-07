33a1717f473d9668d8be0876640317d93512c067
Boni Gopalan <boni@entelligentsia.in>
2020-04-07 13:06:29 +0530

    Adding requestOtp test

---
+++ tests/customers/login/testRequestOtp.js
@@ -0,0 +1,19 @@
+let STAGE = process.env.mygold_stage ? process.env.mygold_stage : 'dev';
+const config = require('../../../config/credentials.json')[STAGE];
+const DvaraGold = require('../../../cliient/dvaragold');
+
+var testGetOtp = async function () {
+    let client = await DvaraGold.Client(config);
+    return client.requestOtp("+919923699356")
+}
+
+testGetOtp()
+.then(result=>{
+    console.dir(result)
+})
+.catch(err=>{
+    console.error(err)
+})
+.finally(()=>{
+    process.exit(0);
+})
