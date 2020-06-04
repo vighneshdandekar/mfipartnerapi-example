84efa99207d5827fa8f854088d9f064e178afb6a
saurabh <saurabh.joshi@dvara.com>
2020-06-04 13:23:55 +0530

    updated addpaymentdetails test case

--- tests/customers/orders/testAddPaymentDetails.js
+++ tests/customers/orders/testAddPaymentDetails.js
@@ -2,8 +2,6 @@ let STAGE = process.env.mygold_stage ? process.env.mygold_stage : 'dev';
 const config = require('../../../config/credentials.json')[STAGE];
 const DvaraGold = require('../../../cliient/dvaragold');
 const details = {
-    
-
         orderIds: ['55f48350-df7b-11e9-a276-61ef2c20eaa7','f146a0e0-df7b-11e9-b140-edb285b0f59e'],
         paymentDate: 'dasd',
         txnReference: '1234',
