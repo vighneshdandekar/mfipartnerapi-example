5c4b3fca72135dfe860b5b0e8afd45e666f6219b
saurabh <saurabh.joshi@dvara.com>
2020-06-04 13:11:12 +0530

    updated addpaymentdetails test case

--- tests/customers/orders/testAddPaymentDetails.js
+++ tests/customers/orders/testAddPaymentDetails.js
@@ -1,23 +1,15 @@
 let STAGE = process.env.mygold_stage ? process.env.mygold_stage : 'dev';
 const config = require('../../../config/credentials.json')[STAGE];
 const DvaraGold = require('../../../cliient/dvaragold');
-const details = [
-    {
+const details = {
+    
 
-        orderId: '55f48350-df7b-11e9-a276-61ef2c20eaa7',
+        orderIds: ['55f48350-df7b-11e9-a276-61ef2c20eaa7','f146a0e0-df7b-11e9-b140-edb285b0f59e'],
         paymentDate: 'dasd',
         txnReference: '1234',
-        paymentInstrumentType: 'cash',
-
-    },    {
-
-        orderId: 'f146a0e0-df7b-11e9-b140-edb285b0f59e',
-        paymentDate: 'dasd',
-        txnReference: '1234',
-        paymentInstrumentType: 'cash',
-
+        paymentInstrumentType: 'NEFT',
     }
-]
+
 
 async function test(){
     let client = await DvaraGold.Client(config);
@@ -28,7 +20,7 @@ test()
     console.dir(result)
 })
 .catch(err=>{
-    console.error(err)
+    console.error(err.data)
 })
 .finally(()=>{
     process.exit(0);
